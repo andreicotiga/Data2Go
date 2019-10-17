de2f7eefabee3def77ae25e24564e4c0b34d185e
Andrei Cotiga <andreicotiga@gmail.com>
2019-10-17 21:35:45 +0300

    fix error thrown and add documetation for ToGo method

--- Data2Go.EntityFrameworkCore/UnitOfWorkExtensions.cs
+++ Data2Go.EntityFrameworkCore/UnitOfWorkExtensions.cs
@@ -5,11 +5,16 @@ namespace Data2Go.EntityFrameworkCore
 {
     public static class UnitOfWorkExtensions
     {
+        /// <summary>
+        /// Creates a new unit of work for the provided context
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns>A new unit of work</returns>
         public static IUnitOfWork ToGo(this DbContext context)
         {
             if (context == null)
             {
-                throw new ArgumentNullException();
+                throw new ArgumentNullException(nameof(context));
             }
 
             return new UnitOfWork(context);
